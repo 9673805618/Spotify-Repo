14eeeefe07600a2650d850230a14488adda45d51
Siddhesh Kumbhar <siddheshkumbhar2017@gmail.com>
2022-07-19 04:02:05 +0530

    first commit

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ let playsong = () =>{
     mysong5.pause();
     mysong6.pause();   
     mysong7.pause();
+    mysong8.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
 }
 const pausesong=()=>{
     mysong1.pause();
@@ -21,6 +28,13 @@ let playsong2 = () =>{
     mysong5.pause();
     mysong6.pause();   
     mysong7.pause();
+    mysong8.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
 }
 const pausesong2=()=>{
     mysong2.pause();
@@ -34,6 +48,13 @@ let playsong3 = () =>{
     mysong5.pause();
     mysong6.pause();   
     mysong7.pause();
+    mysong8.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
 }
 const pausesong3=()=>{
     mysong3.pause();
@@ -47,6 +68,13 @@ let playsong4 = () =>{
     mysong5.pause();
     mysong6.pause();   
     mysong7.pause();
+    mysong8.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
 }
 const pausesong4=()=>{
     mysong4.pause();
@@ -60,6 +88,13 @@ let playsong5 = () =>{
     mysong4.pause();
     mysong6.pause();   
     mysong7.pause();
+    mysong8.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
 }
 const pausesong5=()=>{
     mysong5.pause();
@@ -73,12 +108,18 @@ let playsong6 = () =>{
     mysong4.pause();  
     mysong7.pause();
     mysong1.pause();
+    mysong8.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
 }
 const pausesong6=()=>{
     mysong6.pause();
 }
 var mysong7=new Audio('song/little.mp3')
-var mysong8=new Audio('song/little.mp3')
 let playsong7 = () =>{
     mysong7.play();
     mysong2.pause();
@@ -87,22 +128,545 @@ let playsong7 = () =>{
     mysong4.pause();
     mysong6.pause();   
     mysong5.pause();
+    mysong8.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
 }
 const pausesong7=()=>{
     mysong7.pause();
 
 }
-var mysong8=new Audio('song/little.mp3')
+var mysong8=new Audio('song/mai rohoon.mp3')
 let playsong8 = () =>{
+    mysong8.play();
+    mysong2.pause();
+    mysong3.pause();
+    mysong1.pause();
+    mysong4.pause();
+    mysong6.pause();   
+    mysong5.pause();
+    mysong7.pause();
+    trendsong1.pause();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
+}
+const pausesong8=()=>{
+    mysong8.pause();
+
+}
+
+
+//trending song
+
+var trendsong1=new Audio("song/shamshera.mp3")
+let trendplay1 =()=>{
+    trendsong1.play();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
+    mysong1.pause();
+    mysong2.pause();
+    mysong3.pause();
+    mysong4.pause(); 
+    mysong5.pause();
+    mysong6.pause();   
+    mysong7.pause();
+    mysong8.pause();
+}
+const trendpause1=()=>{
+    trendsong1.pause();
+}
+var trendsong2=new Audio("song/pasoori.mp3")
+let trendplay2 =()=>{
+    trendsong2.play();
+    trendsong1.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
+    mysong1.pause();
+    mysong2.pause();
+    mysong3.pause();
+    mysong4.pause(); 
+    mysong5.pause();
+    mysong6.pause();   
+    mysong7.pause();
+    mysong8.pause();
+}
+const trendpause2=()=>{
+    trendsong2.pause();
+}
+var trendsong3=new Audio("song/shamat.mp3")
+let trendplay3 =()=>{
+    trendsong3.play();
+    trendsong2.pause();
+    trendsong1.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong6.pause();
+    mysong1.pause();
+    mysong2.pause();
+    mysong3.pause();
+    mysong4.pause(); 
+    mysong5.pause();
+    mysong6.pause();   
+    mysong7.pause();
+    mysong8.pause();
+}
+const trendpause3=()=>{
+    trendsong3.pause();
+}
+var trendsong4=new Audio("song/arabic.mp3")
+let trendplay4 =()=>{
+    trendsong4.play();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong1.pause();
+    trendsong5.pause();
+    trendsong6.pause();
+    mysong1.pause();
+    mysong2.pause();
+    mysong3.pause();
+    mysong4.pause(); 
+    mysong5.pause();
+    mysong6.pause();   
+    mysong7.pause();
+    mysong8.pause();
+}
+const trendpause4=()=>{
+    trendsong4.pause();
+}
+var trendsong5=new Audio("song/dil vilan.mp3")
+let trendplay5 =()=>{
+    trendsong5.play();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong1.pause();
+    trendsong6.pause();
+    mysong1.pause();
+    mysong2.pause();
+    mysong3.pause();
+    mysong4.pause(); 
+    mysong5.pause();
+    mysong6.pause();   
+    mysong7.pause();
+    mysong8.pause();
+}
+const trendpause5=()=>{
+    trendsong5.pause();
+}
+var trendsong6=new Audio("song/maiya.mp3")
+let trendplay6 =()=>{
+    trendsong6.play();
+    trendsong2.pause();
+    trendsong3.pause();
+    trendsong4.pause();
+    trendsong5.pause();
+    trendsong1.pause();
+    mysong1.pause();
+    mysong2.pause();
+    mysong3.pause();
+    mysong4.pause(); 
+    mysong5.pause();
+    mysong6.pause();   
+    mysong7.pause();
+    mysong8.pause();
+}
+const trendpause6=()=>{
+    trendsong6.pause();
+}
+
+
+
+//arjit songs
+var arjitsong1=new Audio('song/kesariya.mp3')
+let arjitplay1 =()=>{
+    arjitsong1.play();
+    arjitsong2.pause();
+    arjitsong3.pause();
+    arjitsong4.pause();
+    arjitsong5.pause();
+}
+const arjitpause1 =()=>{
+    arjitsong1.pause();
+
+}
+var arjitsong2=new Audio('song/atak.mp3')
+let arjitplay2 =()=>{
+    arjitsong2.play();
+    arjitsong1.pause();
+    arjitsong3.pause();
+    arjitsong4.pause();
+    arjitsong5.pause();
+}
+const arjitpause2 =()=>{
+    arjitsong2.pause();
+
+}
+var arjitsong3=new Audio('song/khairiyat.mp3')
+let arjitplay3 =()=>{
+    arjitsong3.play();
+    arjitsong2.pause();
+    arjitsong1.pause();
+    arjitsong4.pause();
+    arjitsong5.pause();
+}
+const arjitpause3 =()=>{
+    arjitsong3.pause();
+
+}
+var arjitsong4=new Audio('song/chhod diya.mp3')
+let arjitplay4 =()=>{
+    arjitsong4.play();
+    arjitsong2.pause();
+    arjitsong3.pause();
+    arjitsong1.pause();
+    arjitsong5.pause();
     
 }
+const arjitpause4 =()=>{
+    arjitsong4.pause();
 
-
-
-document.getElementById("themex").onclick= abc;
-
-function abc(){
-    document.getElementById("themex").style.background= "pink";
 }
+var arjitsong5=new Audio('song/mere yara.mp3')
+let arjitplay5 =()=>{
+    arjitsong5.play();
+    arjitsong2.pause();
+    arjitsong3.pause();
+    arjitsong4.pause();
+    arjitsong1.pause();
+}
+const arjitpause5 =()=>{
+    arjitsong5.pause();
+
+}
+
+
+
+
+// neha kakkar song
+
+var nehasong1=new Audio('song/karar.mp3')
+let nehaplay1 =()=>{
+    nehasong1.play();
+    nehasong2.pause();
+    nehasong3.pause();
+    nehasong4.pause();
+    nehasong5.pause();
+}
+const nehapause1 =()=>{
+    nehasong1.pause();
+
+}
+var nehasong2=new Audio('song/yar.mp3')
+let nehaplay2 =()=>{
+    nehasong2.play();
+    nehasong1.pause();
+    nehasong3.pause();
+    nehasong4.pause();
+    nehasong5.pause();
+}
+const nehapause2 =()=>{
+    nehasong2.pause();
+
+}
+var nehasong3=new Audio('song/mashup.mp3')
+let nehaplay3 =()=>{
+    nehasong3.play();
+    nehasong1.pause();
+    nehasong2.pause();
+    nehasong4.pause();
+    nehasong5.pause();
+}
+const nehapause3 =()=>{
+    nehasong3.pause();
+
+}
+var nehasong4=new Audio('song/garmi.mp3')
+let nehaplay4 =()=>{
+    nehasong4.play();
+    nehasong1.pause();
+    nehasong3.pause();
+    nehasong2.pause();
+    nehasong5.pause();
+}
+const nehapause4 =()=>{
+    nehasong4.pause();
+
+}
+var nehasong5=new Audio('song/taaron ke shehar.mp3')
+let nehaplay5 =()=>{
+    nehasong5.play();
+    nehasong1.pause();
+    nehasong3.pause();
+    nehasong4.pause();
+    nehasong2.pause();
+}
+const nehapause5 =()=>{
+    nehasong5.pause();
+
+}
+
+//jubain notiyal song
+
+var jubainsong1=new Audio('song/raton lambiyan.mp3')
+let jubainplay1 =()=>{
+    jubainsong1.play();
+    jubainsong2.pause();
+    jubainsong3.pause();
+    jubainsong4.pause();
+    jubainsong5.pause();
+
+}
+const jubainpause1 =()=>{
+    jubainsong1.pause();
+
+}
+var jubainsong2=new Audio('song/dil galati.mp3')
+let jubainplay2 =()=>{
+    jubainsong2.play();
+    jubainsong1.pause();
+    jubainsong3.pause();
+    jubainsong4.pause();
+    jubainsong5.pause();
+}
+const jubainpause2 =()=>{
+    jubainsong2.pause();
+
+}
+var jubainsong3=new Audio('song/humnava.mp3')
+let jubainplay3 =()=>{
+    jubainsong3.play();
+    jubainsong1.pause();
+    jubainsong2.pause();
+    jubainsong4.pause();
+    jubainsong5.pause();
+}
+const jubainpause3 =()=>{
+    jubainsong3.pause();
+
+}
+var jubainsong4=new Audio('song/tum hi aana.mp3')
+let jubainplay4 =()=>{
+    jubainsong4.play();
+    jubainsong1.pause();
+    jubainsong3.pause();
+    jubainsong2.pause();
+    jubainsong5.pause();
+}
+const jubainpause4 =()=>{
+    jubainsong4.pause();
+
+}
+var jubainsong5=new Audio('song/meri zindagi.mp3')
+let jubainplay5 =()=>{
+    jubainsong5.play();
+    jubainsong1.pause();
+    jubainsong3.pause();
+    jubainsong4.pause();
+    jubainsong2.pause();
+}
+const jubainpause5 =()=>{
+    jubainsong5.pause();
+
+}
+
+// siddhu mooshe wala
+
+var sidhusong1=new Audio('song/295.mp3')
+let sidhuplay1 =()=>{
+    sidhusong1.play();
+    sidhusong2.pause();
+    sidhusong3.pause();
+    sidhusong4.pause();
+    sidhusong5.pause();
+}
+const sidhupause1 =()=>{
+    sidhusong1.pause();
+
+}
+var sidhusong2=new Audio('song/ride.mp3')
+let sidhuplay2 =()=>{
+    sidhusong2.play();
+    sidhusong1.pause();
+    sidhusong3.pause();
+    sidhusong4.pause();
+    sidhusong5.pause();
+}
+const sidhupause2 =()=>{
+    sidhusong2.pause();
+
+}
+var sidhusong3=new Audio('song/levels.mp3')
+let sidhuplay3 =()=>{
+    sidhusong3.play();
+    sidhusong1.pause();
+    sidhusong2.pause();
+    sidhusong4.pause();
+    sidhusong5.pause();
+}
+const sidhupause3 =()=>{
+    sidhusong3.pause();
+
+}
+var sidhusong4=new Audio('song/never fold.mp3')
+let sidhuplay4 =()=>{
+    sidhusong4.play();
+    sidhusong1.pause();
+    sidhusong3.pause();
+    sidhusong2.pause();
+    sidhusong5.pause();
+}
+const sidhupause4 =()=>{
+    sidhusong4.pause();
+
+}
+var sidhusong5=new Audio('song/everybody.mp3')
+let sidhuplay5 =()=>{
+    sidhusong5.play();
+    sidhusong1.pause();
+    sidhusong3.pause();
+    sidhusong4.pause();
+    sidhusong2.pause();
+}
+const sidhupause5 =()=>{
+    sidhusong5.pause();
+
+}
+
+//shreya ghosial song
+
+var sreyasong1=new Audio('song/saibo.mp3')
+let sreyaplay1 =()=>{
+    sreyasong1.play();
+    sreyasong2.pause();
+    sreyasong3.pause();
+    sreyasong4.pause();
+    sreyasong5.pause();
+}
+const sreyapause1 =()=>{
+    sreyasong1.pause();
+
+}
+var sreyasong2=new Audio('song/param.mp3')
+let sreyaplay2 =()=>{
+    sreyasong2.play();
+    sreyasong1.pause();
+    sreyasong3.pause();
+    sreyasong4.pause();
+    sreyasong5.pause();
+}
+const sreyapause2 =()=>{
+    sreyasong2.pause();
+
+}
+var sreyasong3=new Audio('song/heer.mp3')
+let sreyaplay3 =()=>{
+    sreyasong3.play();
+    sreyasong1.pause();
+    sreyasong2.pause();
+    sreyasong4.pause();
+    sreyasong5.pause();
+}
+const sreyapause3 =()=>{
+    sreyasong3.pause();
+
+}
+var sreyasong4=new Audio('song/pal.mp3')
+let sreyaplay4 =()=>{
+    sreyasong4.play();
+    sreyasong1.pause();
+    sreyasong3.pause();
+    sreyasong2.pause();
+    sreyasong5.pause();
+}
+const sreyapause4 =()=>{
+    sreyasong4.pause();
+
+}
+var sreyasong5=new Audio('song/hasi.mp3')
+let sreyaplay5 =()=>{
+    sreyasong5.play();
+    sreyasong1.pause();
+    sreyasong3.pause();
+    sreyasong4.pause();
+    sreyasong2.pause();
+}
+const sreyapause5 =()=>{
+    sreyasong5.pause();
+
+}
+
+//yo yo honey singh
+
+var hunysong1=new Audio('song/de tali.mp3')
+let hunyplay1=()=>{
+    hunysong1.play();
+    hunysong2.pause();
+    hunysong3.pause();
+    hunysong4.pause();
+    hunysong5.pause();
+}
+const hunypause1=()=>{
+    hunysong1.pause();
+}
+var hunysong2=new Audio('song/love dose.mp3')
+let hunyplay2=()=>{
+    hunysong2.play();
+    hunysong1.pause();
+    hunysong3.pause();
+    hunysong4.pause();
+    hunysong5.pause();
+}
+const hunypause2=()=>{
+    hunysong2.pause();
+}
+var hunysong3=new Audio('song/naugthy.mp3')
+let hunyplay3=()=>{
+    hunysong3.play();
+    hunysong2.pause();
+    hunysong1.pause();
+    hunysong4.pause();
+    hunysong5.pause();
+}
+const hunypause3=()=>{
+    hunysong3.pause();
+}
+var hunysong4=new Audio('song/desi.mp3')
+let hunyplay4=()=>{
+    hunysong4.play();
+    hunysong2.pause();
+    hunysong3.pause();
+    hunysong1.pause();
+    hunysong5.pause();
+}
+const hunypause4=()=>{
+    hunysong4.pause();
+}
+var hunysong5=new Audio('song/designer.mp3  ')
+let hunyplay5=()=>{
+    hunysong5.play();
+    hunysong2.pause();
+    hunysong3.pause();
+    hunysong4.pause();
+    hunysong1.pause();
+}
+const hunypause5=()=>{
+    hunysong5.pause();
+}
+
+
+
+
 
 
